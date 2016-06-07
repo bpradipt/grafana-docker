584afc740236f1bb7898382a24f1bd0c65ab2a0d
Pradipta Kr. Banerjee <pradipta.banerjee@gmail.com>
2016-06-07 18:37:50 +0000

    Build grafana 2.6.0 docker image on Power

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ _grafana_version=$1
 _grafana_tag=$2
 _release_build=false
 
+ARCH=$( uname -m )
 if [ -z "${_grafana_version}" ]; then
 	source GRAFANA_VERSION
 	_grafana_version=$GRAFANA_VERSION
@@ -14,8 +15,20 @@ echo "GRAFANA_VERSION: ${_grafana_version}"
 echo "DOCKER TAG: ${_grafana_tag}"
 echo "RELEASE BUILD: ${_release_build}"
 
-docker build --build-arg GRAFANA_VERSION=${_grafana_version} --tag "grafana/grafana:${_grafana_tag}"  --no-cache=true .
+if [ ${ARCH} == "x86_64" ]
+then
+	docker build --build-arg GRAFANA_VERSION=${_grafana_version} \
+		--tag "grafana/grafana:${_grafana_tag}" -f Dockerfile .
+elif [ ${ARCH} == "ppc64le" ]
+then
+	docker build --tag "grafana/grafana:${_grafana_tag}" -f Dockerfile.ppc64le .
+fi
 
 if [ $_release_build == true ]; then
-	docker build --build-arg GRAFANA_VERSION=${_grafana_version} --tag "grafana/grafana:latest"  --no-cache=true .
+	if [ ${ARCH} == "x86_64" ]; then
+		docker build --build-arg GRAFANA_VERSION=${_grafana_version} \
+			--tag "grafana/grafana:latest" -f Dockerfile .
+	elif [ ${ARCH} == "ppc64le" ]; then
+		docker build --tag "grafana/grafana:latest" -f Dockerfile.ppc64le .
+	fi
 fi
